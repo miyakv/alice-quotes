1164829b923dcbab2d8567eb4de301d702962308
Kamil <45203116+ulyanovskk@users.noreply.github.com>
2019-04-30 20:08:38 +0400

    Подсказки
    Подсказки

--- dialog-runner.py
+++ dialog-runner.py
@@ -29,6 +29,15 @@ def main():
     return json.dumps(response)
 
 
+def check_help(req):
+    if req['request']['original_utterance'].lower() in ['помощь', "что делать?", "что делать"]:
+        return 1
+    elif req['request']['original_utterance'].lower() in ["что ты умеешь", "что ты умеешь?"]:
+        return 2
+    else:
+        return 0
+
+
 def try_again(res, btns):
     res['response']['text'] = "Попробуй ещё раз"
     res['response']['buttons'] = btns
@@ -36,6 +45,10 @@ def try_again(res, btns):
 
 def function_manager(res, req):
     user_id = req['session']['user_id']
+    if check_help(req) == 2:
+        res['response']['text'] = 'Я могу выполнить одну из задача: найти цитату по автору,' \
+                                      'прислать случайную цитату или цитату дня. Введи то, что я предложила ранее.'
+        return
     if req['request']['original_utterance'].lower() == "цитата по автору" or \
             sessionStorage[user_id]["status"] == 1:
         get_quote_by_author(res, req)
@@ -43,15 +56,11 @@ def function_manager(res, req):
         day_quote(res, req)
     elif req['request']['original_utterance'].lower() == 'случайная цитата':
         random_quote(res, req)
+
     else:
         logging.info('403 ' + req['request']['original_utterance'])
         res['response']['text'] = '*{}*'.format(
             req['request']['original_utterance'].lower())
-
-
-def get_help(res):
-    res['response']['text'] = 'Тебе никто не поможет, вухвхвахаха.  \
-                              Лучше просто ответь на предыдущий вопрос'
 
 
 def day_quote(res, req):
@@ -112,6 +121,7 @@ def day_quote_menu(res):
 
 def handle_dialog(res, req):
     user_id = req['session']['user_id']
+
     if req['session']['new']:
         res['response'][
             'text'] = 'Привет! Меня зовут Алиса. И я не люблю разговаривать ' \
@@ -131,9 +141,14 @@ def handle_dialog(res, req):
 
     if sessionStorage[user_id]["first_name"] is None:
         first_name = get_first_name(req)
-        if req['request']['original_utterance'].lower() == 'помощь':
-            get_help(res)
+        if check_help(req) == 2:
+            res['response']['text'] = 'Я могу выполнить одну из задача: найти цитату по автору,' \
+                                  'прислать случайную цитату или цитату дня. Напиши своё имя.'
             return
+        if check_help(req) == 1:
+            res['response']['text'] = 'Напиши своё имя.'
+            return
+
         elif first_name is None:
             res['response']['text'] = 'Не расслышала имя. Повтори, пожалуйста!'
 
@@ -151,6 +166,15 @@ def handle_dialog(res, req):
                                                                 "русский"]:
                 lang = req['request']['original_utterance'].lower()
                 sessionStorage[user_id]['language'] = translate(lang)
+            if check_help(req) == 2:
+                res['response']['text'] = 'Я могу выполнить одну из задача: найти цитату по автору,' \
+                                      'прислать случайную цитату или цитату дня. Выбери язык, на котором я буду присылать тебе цитаты.'
+                choose_language(res)
+                return
+            elif check_help(req) == 1:
+                res['response']['text'] = 'Выбери язык, на котором я буду присылать тебе цитаты.'
+                choose_language(res)
+                return
             else:
                 logging.info(req['request']['original_utterance'].lower())
                 try_again(res, [
@@ -179,6 +203,12 @@ def handle_dialog(res, req):
     else:
         options = ['цитата по автору', 'случайная цитата', 'цитата дня']
         if sessionStorage[user_id]['language'] is None:
+            if check_help(req) == 2:
+                res['response']['text'] = 'Я могу выполнить одну из задача: найти цитату по автору,' \
+                                      'прислать случайную цитату или цитату дня. Выбери язык, на котором я ' \
+                                          'буду присылать тебе цитаты.'
+                choose_language(res)
+                return
             if req['request']['original_utterance'].lower() in ["английский",
                                                                 "испанский",
                                                                 "русский"]:
@@ -208,6 +238,10 @@ def handle_dialog(res, req):
                         'hide': True
                     }
                 ]
+                return
+            elif check_help(req) == 1:
+                res['response']['text'] = 'Выбери язык, на котором я буду присылать тебе цитаты.'
+                choose_language(res)
                 return
             else:
                 logging.info(req['request']['original_utterance'].lower())
@@ -240,8 +274,16 @@ def handle_dialog(res, req):
         elif req['request']['original_utterance'].lower() in options or \
                 sessionStorage[user_id]["status"] != 0:
             function_manager(res, req)
-        elif req['request']['original_utterance'].lower() == "помощь":
-            get_help(res)
+        elif check_help(req) == 2:
+            res['response']['text'] = 'Я могу выполнить одну из задача: найти цитату по автору,' \
+                                      'прислать случайную цитату или цитату дня. Выбери задачу, а я её решу!'
+            main_menu(res)
+
+        elif check_help(req) == 1:
+            res['response']['text'] = 'Нажав на кнопку "Цитата по автору", ты сможешь выбрать автора, и я пришлю его цитату' \
+                                      ' Нажав на кнопку "Случайная цитата", ты получишь случайную цитату.' \
+                                      ' Нажав на кнопку "Цитата дня", ты получишь цитату дня с главной страницы Викицитатника в зависимости от выбранного языка.' \
+                                      ' Нажав на кнопку "Сменить язык", ты сможешь заново выбрать язык цитат.'
             main_menu(res)
             return
         else:
@@ -419,7 +461,8 @@ def get_quote_by_author(res, req):
     elif sessionStorage[user_id]["status"] == 1:
         data = req['request']['original_utterance'].lower()
         if data not in ["ещё цитату", "сменить автора", "в меню", "помощь",
-                        "кто это?"]:
+                        "кто это?", "что делать"]:
+
             data = translate(data)
             sessionStorage[user_id]["author"] = data
             lang = sessionStorage[user_id]['language']
@@ -451,10 +494,56 @@ def get_quote_by_author(res, req):
                     'hide': True
                 }]
         else:
-            if data == "помощь":
-                get_help(res)
-                return
-            elif data == "сменить автора":
+            if check_help(req) == 1:
+                if not sessionStorage[user_id]["author"]:
+                    res['response']['text'] = 'Выбери автора цитаты'
+                    res['response']['buttons'] = [
+                    {
+                        'title': "Путин",
+                        'hide': True
+                    },
+                    {
+                        'title': 'Ницше',
+                        'hide': True
+                    },
+                    {
+                        'title': 'Хайям',
+                        'hide': True
+                    },
+                    {
+                        'title': 'Ганди',
+                        'hide': True
+                    }]
+                    return
+                else:
+                    res['response']['text'] = 'Нажав на кнопку "Кто это?", ты получишь информацию про автора.' \
+                                  ' Нажав на кнопку "Ещё цитату", ты получишь цитату выбранного ранее автора.' \
+                                  ' Нажав на кнопку "Сменить автора", ты сможешь заново выбрать автора.' \
+                                  ' Нажав на кнопку "В меню", ты выйдешь в главное меню.'
+                    res['response']['buttons'] = [
+                    {
+                        'title': "Кто это?",
+                        'hide': True
+                    },
+                    {
+                        'title': "Ещё цитату",
+                        'hide': True
+                    },
+                    {
+                        'title': 'Сменить автора',
+                        'hide': True
+                    },
+                    {
+                        'title': 'В меню',
+                        'hide': True
+                    },
+                    {
+                        'title': 'Помощь',
+                        'hide': True
+                    }
+                    ]
+                    return
+            if data == "сменить автора":
                 sessionStorage[user_id]["status"] = 0
                 get_quote_by_author(res, req)
             elif data == "кто это?":
